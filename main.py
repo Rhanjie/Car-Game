c1f6a7c9d9d153b58a88f0cdc95c5f25b47de24f
RhAnjiE <irhanjie@gmail.com>
2024-01-14 04:31:57 +0100

    Finish roads and movement

--- main.py
+++ main.py
@@ -25,6 +25,23 @@ class Object(ABC):
         return current_y - self.y > 800 # 100 - 150
 
 
+class Line(Object):
+    def __init__(self, x: int, y: int, color: str):
+        Object.__init__(self, x, y, color)
+
+        self.width = 5
+        self.height = 60
+
+    def update(self, current_y: int):
+        pass
+
+    def draw(self, canvas: Canvas, current_y: int):
+        y_screen = current_y - self.y
+
+        square = canvas.create_rectangle(
+            self.x, y_screen, self.x + self.width, y_screen + self.height, fill=self.color)
+
+
 class Obstacle(Object):
     def __init__(self, x: int, y: int, color: str):
         Object.__init__(self, x, y, color)
@@ -69,10 +86,10 @@ class Player(Car):
 
     def change_direction(self, direction: Direction):
         if direction == RIGHT:
-            self.x += 20
+            self.x += 10
 
         elif direction == LEFT:
-            self.x -= 20
+            self.x -= 10
 
     def draw(self, canvas: Canvas, current_y: int):
         square = canvas.create_rectangle(
@@ -82,18 +99,39 @@ class Player(Car):
 class Terrain:
     def __init__(self, lines_color: str, road_width: int, screen_width: int, screen_height: int):
         self.lines_color = lines_color
+        self.background_items = []
         self.obstacles = []
 
         self.road_width = road_width
         self.screen_width = screen_width
         self.screen_height = screen_height
 
-        self.total_spawned = 0
+        self.total_obstacles_spawned = 0
+        self.total_centered_lines_spawned = -4
+
+        self.spawn_road_cropped_line(-600)
+        self.spawn_road_cropped_line(-450)
+        self.spawn_road_cropped_line(-300)
+        self.spawn_road_cropped_line(-150)
 
     def update(self, current_y: int):
+        self.background_items[:] = [item for item in self.background_items if not item.ready_to_delete(current_y)]
         self.obstacles[:] = [obstacle for obstacle in self.obstacles if not obstacle.ready_to_delete(current_y)]
 
-        should_spawn = (current_y / 200 > self.total_spawned)
+        self.spawn_obstacle(current_y)
+        self.spawn_road_cropped_line(current_y)
+
+    def draw(self, canvas: Canvas, current_y: int):
+        for item in self.background_items:
+            item.draw(canvas, current_y)
+
+        for obstacle in self.obstacles:
+            obstacle.draw(canvas, current_y)
+
+        self.draw_road_side_lines(canvas)
+
+    def spawn_obstacle(self, current_y: int):
+        should_spawn = (current_y / 200 > self.total_obstacles_spawned)
         if not should_spawn:
             return
 
@@ -101,17 +139,27 @@ class Terrain:
 
         position_x = random.randint(offset, self.screen_width - offset)
         self.obstacles.append(Obstacle(position_x, current_y, "#FFFF00"))
-        self.total_spawned += 1
+        self.total_obstacles_spawned += 1
 
-    def draw(self, canvas: Canvas, current_y: int):
-        for obstacle in self.obstacles:
-            obstacle.draw(canvas, current_y)
+    def spawn_road_cropped_line(self, current_y: int):
+        should_spawn = (current_y / 150 > self.total_centered_lines_spawned)
+        if not should_spawn:
+            return
 
-        # draw centered lines
+        position_x = self.screen_width / 2
+        self.obstacles.append(Line(position_x, current_y, self.lines_color))
+        self.total_centered_lines_spawned += 1
 
-        # draw side lines
+    def draw_road_side_lines(self, canvas: Canvas):
+        offset = int((self.screen_width - self.road_width) / 2) - 30
+        side_line_x1 = offset
+        side_line_x2 = self.screen_width - offset
 
-        pass
+        canvas.create_rectangle(
+            side_line_x1, 0, side_line_x1 + 5, self.screen_height, fill=self.lines_color)
+
+        canvas.create_rectangle(
+            side_line_x2, 0, side_line_x2 + 5, self.screen_height, fill=self.lines_color)
 
 
 class GameManager:
@@ -125,7 +173,7 @@ class GameManager:
         self.canvas = Canvas(self.window, bg=color, height=height, width=width)
         self.canvas.pack()
 
-        self.terrain = Terrain("#ffffff", 500, width, height)
+        self.terrain = Terrain("#ffffff", 300, width, height)
 
         self.window.update()
 
@@ -164,6 +212,6 @@ if __name__ == '__main__':
     height = 600
 
     game_manager = GameManager(800, 600, "#000000")
-    player = Player(int(width / 2), 0, "#00FF00", 200, 500)
+    player = Player(int(width / 2), 0, "#00FF00", 300, 500)
 
     game_manager.main_loop(player)
